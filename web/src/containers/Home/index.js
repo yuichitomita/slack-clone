e977f6b0760ad9af8942b824e3ce2dcdc3ef98c7
yuichitomita <y.tomita@speedlink-td.info>
2017-06-14 23:23:37 +0900

    fix react-router-dom v3 -> v4

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { logout } from '../../actions/session';
 import Navbar from '../../components/Navbar';
 
